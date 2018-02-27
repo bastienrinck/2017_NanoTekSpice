e633904b294810ad880d9af44517f189b6075409
Bastien <bastien.rinck@epitech.eu>
2018-02-27 11:48:10 +0100

    merge

--- TekSpice.hpp
+++ TekSpice.hpp
@@ -24,22 +24,22 @@
 
 class TekSpice {
 	public:
-		std::unique_ptr <nts::IComponent> createComponent(const std::string &, const std::string &);
+		std::shared_ptr <nts::IComponent> createComponent(const std::string &, const std::string &);
 
 		TekSpice();
 		~TekSpice();
 	protected:
 	private:
-		std::map<std::string, std::unique_ptr<nts::IComponent>(*)(const std::string &)> _fcts = {
-			{"input", [](const std::string &){return std::unique_ptr<nts::IComponent>(new Component_Input);}},
-			{"output", [](const std::string &){return std::unique_ptr<nts::IComponent>(new Component_Output);}},
-			{"4001", [](const std::string &){return std::unique_ptr<nts::IComponent>(new Component_4001);}},
-			{"4008", [](const std::string &){return std::unique_ptr<nts::IComponent>(new Component_4008);}},
-			{"4011", [](const std::string &){return std::unique_ptr<nts::IComponent>(new Component_4011);}},
-			{"4011", [](const std::string &){return std::unique_ptr<nts::IComponent>(new Component_4030);}},
-			{"4011", [](const std::string &){return std::unique_ptr<nts::IComponent>(new Component_4069);}},
-			{"4011", [](const std::string &){return std::unique_ptr<nts::IComponent>(new Component_4071);}},
-			{"4011", [](const std::string &){return std::unique_ptr<nts::IComponent>(new Component_4081);}}
+		std::map<std::string, std::shared_ptr<nts::IComponent>(*)(const std::string &)> _fcts = {
+			{"input", [](const std::string &){return std::shared_ptr<nts::IComponent>(new Component_Input);}},
+			{"output", [](const std::string &){return std::shared_ptr<nts::IComponent>(new Component_Output);}},
+			{"4001", [](const std::string &){return std::shared_ptr<nts::IComponent>(new Component_4001);}},
+			{"4008", [](const std::string &){return std::shared_ptr<nts::IComponent>(new Component_4008);}},
+			{"4011", [](const std::string &){return std::shared_ptr<nts::IComponent>(new Component_4011);}},
+			{"4011", [](const std::string &){return std::shared_ptr<nts::IComponent>(new Component_4030);}},
+			{"4011", [](const std::string &){return std::shared_ptr<nts::IComponent>(new Component_4069);}},
+			{"4011", [](const std::string &){return std::shared_ptr<nts::IComponent>(new Component_4071);}},
+			{"4011", [](const std::string &){return std::shared_ptr<nts::IComponent>(new Component_4081);}}
 		};
 };
 
