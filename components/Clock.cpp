dff64bed67fc86fded2ee3370121e9f899863987
Thibaut Cornolti <thibaut@cornolti.eu>
2018-03-02 14:52:09 +0100

    o

---
+++ components/Clock.cpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2018
+** TekSpice
+** File description:
+** oui
+*/
+
+#include "Clock.hpp"
+
+Component_Clock::Component_Clock()
+	: _outPin(nts::UNDEFINED)
+{
+}
+
+nts::Tristate Component_Clock::compute(std::size_t pin)
+{
+	if (pin != 1)
+		throw std::out_of_range("pin out of authorized range");
+	_outPin = (_outPin == nts::TRUE) ? nts::FALSE : nts::TRUE;
+	return _outPin;
+}
+
+nts::Tristate &Component_Clock::getPin(std::size_t pin)
+{
+	if (pin != 1)
+		throw std::out_of_range("pin out of authorized range");
+	return _outPin;
+}
+
+void Component_Clock::setLink(std::size_t, nts::IComponent &,
+				std::size_t)
+{
+	throw std::out_of_range("pin out of authorized range");
+}
+
+void Component_Clock::dump() const
+{
+	std::cout 
+		<< "=== Clock ===" << std::endl
+		<< "Pin 1: " << _outPin << std::endl
+		<< "=============" << std::endl;
+}
---
+++ components/Clock.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2018
+** TekSpice
+** File description:
+** oui
+*/
+
+#pragma once
+
+#include <vector>
+#include <map>
+#include "../IComponent.hpp"
+#include "../Logic.hpp"
+
+class Component_Clock : public nts::IComponent {
+	public:
+		Component_Clock();
+		~Component_Clock() = default;
+
+		nts::Tristate compute(std::size_t = 1) final;
+		void setLink(std::size_t, nts::IComponent &, std::size_t) final;
+		nts::Tristate &getPin(std::size_t) final;
+		void dump() const final;
+		nts::C_TYPE getType() { return nts::C_INPUT; }
+	protected:
+	private:
+		nts::Tristate _outPin;
+};
